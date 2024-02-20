e5b7964e35a590d65750e4ba8c25973c083b4ddf
Christine Hoang <christinehoangt@gmail.com>
2024-02-20 14:23:56 -0600

    saving changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
     <nav className = "w-full flex py-6 justify-between items-center navbar">
       <img src = {logo} alt = "hoobank" className = "w-[124px] h-[32px]"/>
 
-      <ul className = "list-non sm:flex hidden justify-end items-center flex-1">
+      <ul className = "list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index)=>(
           <li 
           key = {nav.id} 
@@ -36,7 +36,20 @@ const NavBar = () => {
            className = {`${toggle ? 'flex' : 'hidden'}
            p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
            >
-
+            <ul className = "list-none flex flex-col justify-end items-center flex-1">
+              {navLinks.map((nav, index)=>(
+                <li 
+                key = {nav.id} 
+                className = {`font-poppins font-normal cursor-pointer text-[16px] 
+                ${index=== navLinks.length -1 ? 'mr-0' : 'mb-4'} text-white mr-10`}
+                >
+                <a href = {`#${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+            
            </div>
       </div>
     </nav>
